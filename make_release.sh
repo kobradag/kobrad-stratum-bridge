cb95bb143eff69fe40968465e753d8c4b62e9e53
kobradag <168386458+kobradag@users.noreply.github.com>
2024-09-24 09:57:14 -0500

    Update make_release.sh

--- make_release.sh
+++ make_release.sh
@@ -3,9 +3,9 @@ rm -rf release
 mkdir -p release
 cd release
 VERSION=0.13.4
-ARCHIVE="py_bridge-${VERSION}"
-OUTFILE="py_bridge"
-OUTDIR="py_bridge"
+ARCHIVE="kobrad_bridge-${VERSION}"
+OUTFILE="kobrad_bridge"
+OUTDIR="kobrad_bridge"
 
 # windows
 mkdir -p ${OUTDIR};env GOOS=windows GOARCH=amd64 go build -o ${OUTDIR}/${OUTFILE}.exe ${CMD_PATH};cp ${CMD_PATH}/config.yaml ${OUTDIR}/
